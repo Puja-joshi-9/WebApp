f6d5d02d352d192d53f203f968bc37e10aee5bd8
Puja Bhatt <pujabhatt@Pujas-MacBook-Air.local>
2020-07-13 10:24:15 +0545

    edit header

---
+++ src/Components/Shared/Header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+function Header() {
+
+    const headerStyle={backgroundColor:'blue'
+    
+
+     }
+    return (
+        <>
+            <div style={headerStyle}>  <i class="shopping_basket"></i>
+ Shopping Cart</div>
+        </>
+    )
+}
+
+export default Header
